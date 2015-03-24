8ee8303fce028bda021b943926561911bff411f3
emilefokkema <efokkema@sogyo.nl>
2015-03-24 14:28:08 +0100

    nieuwe dingen

--- blockly/custom_blocks/generator_loopnaar.js
+++
@@ -1,7 +0,0 @@
-Blockly.JavaScript['loop_naar'] = function(block) {
-  var dropdown_dir = block.getFieldValue('DIR');
-  var value_dist = Blockly.JavaScript.valueToCode(block, 'DIST', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = 'loop_'+(dropdown_dir=='L'?'links':'rechts')+'('+value_dist+')';
-  return code;
-};
